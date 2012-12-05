e1dee6113d54491dd0999f7b9d4c6aac73ff9f3b
Derek Lindahl <dlindahl@customink.com>
2012-12-05 14:27:23 -0500

    Make the `reset!` method callable...

--- lib/stuffed_bunny.rb
+++ lib/stuffed_bunny.rb
@@ -4,7 +4,7 @@ require 'stuffed_bunny/bunny_overrides'
 module StuffedBunny
 
   # Call this in a test's setup method to stub the Bunny gem.
-  def reset!
+  def self.reset!
     Bunny.reset_exchanges
   end
 
