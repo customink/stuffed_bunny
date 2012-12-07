ee6cca194d9d43f6b966af1cb3c922dc75b543a2
Christopher R. Murphy <chmurph2+git@gmail.com>
2012-12-07 17:09:32 -0500

    Removed extraneous whitespace.

--- lib/stuffed_bunny.rb
+++ lib/stuffed_bunny.rb
@@ -2,10 +2,8 @@ require 'stuffed_bunny/version'
 require 'stuffed_bunny/bunny_overrides'
 
 module StuffedBunny
-
   # Call this in a test's setup method to stub the Bunny gem.
   def self.reset!
     Bunny.reset_exchanges
   end
-
 end
