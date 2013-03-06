24679f8afd6ab53e0c4abe274dc660cada76d63f
Christopher R. Murphy <chmurph2+git@gmail.com>
2013-03-06 13:22:26 -0500

    Changed misleading comment.

--- lib/stuffed_bunny.rb
+++ lib/stuffed_bunny.rb
@@ -2,7 +2,7 @@ require 'stuffed_bunny/version'
 require 'stuffed_bunny/bunny_overrides'
 
 module StuffedBunny
-  # Call this in a test's setup method to stub the Bunny gem.
+  # Call this in a test's teardown method to clear messages in the exchanges.
   def self.reset!
     Bunny.reset_exchanges
   end
